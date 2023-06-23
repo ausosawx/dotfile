31f34e11159f34be5ccc65e3098af9c2ae902bf5
ausosawx <ausosa@tuta.io>
2023-06-23 18:46:46 +0800

    update

--- rofi/leave/leave.sh
+++ rofi/leave/leave.sh
@@ -2,7 +2,7 @@
 
 choice=$(printf "Lock\nLogout\nSuspend\nReboot\nShutdown\nHibernate" | rofi -dmenu)
 if [[ $choice == "Lock" ]];then
-    bash ~/.config/system_scripts/wayland_session_lock
+    bash ~/.config/scripts/wayland_session_lock
 elif [[ $choice == "Logout" ]];then
     pkill -kill -u "$USER"
 elif [[ $choice == "Suspend" ]];then
---
+++ scripts/systemupdate.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Check release
+if [ ! -f /etc/arch-release ] ; then
+    exit 0
+fi
+
+# Check for updates
+ofc=$(paru -Qua | wc -l)
+
+# Calculate total available updates
+upd=$((ofc))
+echo "$upd"
+
+# Show tooltip
+if [ $upd -eq 0 ] ; then
+    echo " Packages are up to date"
+else
+    echo "󱓽 Official $ofc"
+fi
+
+# Trigger upgrade
+if [ "$1" == "up" ] ; then
+    foot --title systemupdate sh -c 'paru -Syu'
+fi
+
